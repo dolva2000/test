039ca11b9631fc7b372efceee58ae7e30b4eb3db
dolva2000 <80533885+dolva2000@users.noreply.github.com>
2023-11-22 17:02:28 +0100

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ function MyApp({ Component, pageProps }) {
       <Head>
         <meta charSet="utf-8" />
 
-        <title>Dolva - Multipurpose Bootstrap Template</title>
+        <title>Dolva - Multipurpose  Template</title>
 
         <meta name="description" content="" />
     <meta name="description" content="" />
